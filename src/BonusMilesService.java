50b62e759c7a5bc605274f167e2acda15b8033fc
Азат Ильдарханов <defcon797@gmail.com>
2022-04-26 22:11:34 +0300

    delete comment

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,6 +1,5 @@
 public class BonusMilesService {
     public int calculate(int cost) {
-        //int ticketPrice = 190;
         int bonusPerMile = 20;
         int bonusMiles = cost / bonusPerMile;
 
